723562559ca2aaa449a21ed7e857ed14c9b586bd
Enzaoooo <enzosouza6485@gmail.com>
2024-08-20 15:55:41 -0300

    criando .env

--- src/server.js
+++ src/server.js
@@ -7,6 +7,6 @@ const app = express()
 app.use('/auth', authRouter)
 app.use('/account', accountRouter)
 
-app.listen(3000, () => {
-    console.log('Servidor Rodando em http://localhost:3000')
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor Rodando no ambiente  ${ process.env.AMBIENTE }`)
 })
